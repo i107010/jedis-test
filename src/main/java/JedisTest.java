cd6b5838d284416594af5e662dd5d7592345dc96
Yangzhi <pococksivt@gmail.com>
2021-08-15 14:20:34 +0800

    v4.0

--- src/main/java/JedisTest.java
+++ src/main/java/JedisTest.java
@@ -2,16 +2,16 @@ import redis.clients.jedis.Jedis;
 
 public class JedisTest {
     public static void main(String[] args) {
-//        Jedis jedis = new Jedis("hadoop112", 6379);
+        Jedis jedis = new Jedis("hadoop112", 6379);
 
-//        jedis.set("aaa", "1");
+        jedis.set("aaa", "1");
 
-//        System.out.println(jedis.get("aaa"));
+        System.out.println(jedis.get("aaa"));
 
 //        commit v2.0
         System.out.println("v2.0");
 
-//        jedis.close();
+        jedis.close();
 
 
         System.out.println("hot-fix v2.1");
